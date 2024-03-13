618c08a9d02f45344f85376a78f208be003f6068
waweroh <scadmsez@gmail.com>
2024-03-13 17:37:58 +0300

    created models and made migrations

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employees (models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  photo = models.ImageField(upload_to='images')
+  designation = models.CharField(max_length=100)
+  email_address = models.EmailField(max_length=100, unique=True)
+  phone_number = models.CharField(max_length=12, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
