4a32129ab114947a5fed12e5319123ff4d625d4a
waweroh <scadmsez@gmail.com>
2024-03-14 18:14:18 +0300

    add name path for url referencing

--- employees/urls.py
+++ employees/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('<int:pk>/', views.employee_detail),
+  path('<int:pk>/', views.employee_detail, name='employee_detail'),
 ]
 
