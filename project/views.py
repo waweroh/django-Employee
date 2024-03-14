026ccf59d0e1107c697817361a4dbd621c6f50d9
waweroh <scadmsez@gmail.com>
2024-03-14 14:19:32 +0300

    modify function to display employee objects

--- project/views.py
+++ project/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employees
 
 def home(request):
-    return render(request,'home.html')
+    employees = Employees.objects.all()
+    context = {
+        'employees':employees,
+    }
+    return render(request,'home.html', context)
