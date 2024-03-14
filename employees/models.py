c6ef19d32c53982a66b847a27e37a074d781522e
waweroh <scadmsez@gmail.com>
2024-03-14 14:26:47 +0300

    display first name on the db

--- employees/models.py
+++ employees/models.py
@@ -10,3 +10,6 @@ class Employees (models.Model):
   phone_number = models.CharField(max_length=12, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.first_name
