12af30c884002f051caf8ff4855259314f67990f
waweroh <scadmsez@gmail.com>
2024-03-14 16:57:00 +0300

    display employee by id

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from employees.models import Employees
+from django.http import Http404, HttpResponse
+
 
 # Create your views here.
+
+def employee_detail(request, pk):
+  # try:
+  #   employee = Employees.objects.get(pk=pk)
+  #   print(employee)
+  # except:
+  #   raise Http404
+  employee = get_object_or_404(Employees, pk=pk)
+  return HttpResponse(employee)
